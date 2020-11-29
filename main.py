0d2d15669ad0c173c36506caf5dcd0857612e617
Graham Kemp <grahamk206@gmail.com>
2020-11-29 17:16:56 +0000

    black formatting

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from typing import List
 DCL_SOCKET: int = 7000
 API_KEY: List[bytes] = b"hello world"
 
+
 def heartbeat():
 
     while True:
@@ -15,7 +16,8 @@ def heartbeat():
 
         if data == "Alive":
             stream.send(b"Alive\0")
-            
+
+
 stream = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 stream.connect(("127.0.0.1", DCL_SOCKET))
 stream.send(API_KEY)
