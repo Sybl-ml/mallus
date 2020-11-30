43ebf420551a5dbad69d9357de55542504250325
Graham Kemp <grahamk206@gmail.com>
2020-11-30 15:46:16 +0000

    complete authentication CLI

--- authenticate.py
+++ authenticate.py
@@ -16,32 +16,38 @@ stream.connect(("127.0.0.1", DCL_SOCKET))
 
 def sign_challenge(challenge: List[bytes], private_key):
 
-    private_key = crypto.load_privatekey(private_key)
-    return crypto.sign(private_key, challenge)
+    private_key = crypto.load_privatekey(crypto.FILETYPE_PEM, private_key)
+    return crypto.sign(private_key, challenge, "sha256")
 
 
 def parse_message(message: Dict) -> Tuple[str, Any]:
 
+    # print(message)
     keys: List[str] = list(message)
 
     if len(keys) > 1:
-        raise Exception
+        raise IndexError
 
     variant: str = keys[0]
-    data: Any = message[type]
+    data: Any = message[variant]
 
     return (variant, data)
 
 
-def authenticate_challenge(challenge: str):
+def authenticate_challenge(challenge: Dict, email: str, model_name: str):
 
+    challenge = challenge["challenge"]
+    print("Authenticating challenge")
     try:
         private_key = os.environ["PRIVATE_KEY"]
+        # print(private_key)
         signed_challenge = sign_challenge(base64.b64decode(challenge), private_key)
 
-        message: str = "{{ signed_challenge: {} }}".format(signed_challenge)
-
-        stream.send(message)
+        message = {"ChallengeResponse": {"email": email, "model_name": model_name, "response": base64.b64encode(signed_challenge).decode('utf-8') }}
+        
+        # print(message)
+        stream.send(json.dumps(message).encode('utf-8'))
+        print("Authenticated")
     except KeyError:
         print("Private Key not set in environemt variable")
 
@@ -49,8 +55,8 @@ def authenticate_challenge(challenge: str):
 def display_access(data: Dict):
 
     try:
-        access_token: str = data["access_token"]
-        model_id: str = data["model_id"]
+        access_token: str = data["token"]
+        model_id: str = data["id"]
 
         print("ACCESS TOKEN: {} \n MODEL ID: {}".format(access_token, model_id))
     except KeyError:
@@ -58,14 +64,14 @@ def display_access(data: Dict):
 
 
 def verify(email: str, name: str) -> bool:
-    message = {"email": email, "model_name": name}
-    stream.send(message)
+    message = {"NewModel": {"email": email, "model_name": name}}
+    stream.send(json.dumps(message).encode('utf-8'))
 
     while True:
         # Read some data
-        data = json.loads(stream.recv(1024)[:-1])
+        data = json.loads(stream.recv(1024))
 
-        print("data: {}".format(data))
+        # print("data: {}".format(data))
 
         try:
             variant, data = parse_message(data)
@@ -73,13 +79,13 @@ def verify(email: str, name: str) -> bool:
             if variant == "Alive":
                 stream.send(b"Alive\0")
             elif variant == "Challenge":
-                authenticate_challenge(data)
-            elif variant == "Identifier":
+                authenticate_challenge(data, email, name)
+            elif variant == "AccessToken":
                 display_access(data)
             else:
                 print("Unknown message Variant")
 
-        except Exception:
+        except IndexError:
             print("Message cannot be parsed")
 
 
@@ -90,3 +96,7 @@ def main():
     print("Authenticating...")
 
     verify(email, name)
+    return
+
+if __name__ == "__main__":
+    main()
