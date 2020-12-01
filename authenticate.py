545107255caa2c6339e627f5e1608b5962fb192e
Graham Kemp <grahamk206@gmail.com>
2020-12-01 17:11:27 +0000

    Black Formatting and saving Access token

--- authenticate.py
+++ authenticate.py
@@ -1,3 +1,4 @@
+from enum import Enum
 import socket
 from typing import Any, Dict, List, Tuple
 import json
@@ -6,96 +7,137 @@ import base64
 
 from OpenSSL import crypto
 from dotenv import load_dotenv
+from xdg import xdg_data_home
+
 load_dotenv()
 
 DCL_SOCKET: int = 7000
-
-stream = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-stream.connect(("127.0.0.1", DCL_SOCKET))
+DCL_IP: str = "127.0.0.1"
 
 
-def sign_challenge(challenge: List[bytes], private_key):
+class Authenication:
+    def __init__(self, email, model_name):
+        self.stream = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.stream.connect(("127.0.0.1", DCL_SOCKET))
 
-    private_key = crypto.load_privatekey(crypto.FILETYPE_PEM, private_key)
-    return crypto.sign(private_key, challenge, "sha256")
+        self.email = email
+        self.model_name = model_name
 
+        self.access_token = None
+        self.model_id = None
 
-def parse_message(message: Dict) -> Tuple[str, Any]:
+    def sign_challenge(self, challenge: List[bytes], private_key):
 
-    # print(message)
-    keys: List[str] = list(message)
+        private_key = crypto.load_privatekey(crypto.FILETYPE_PEM, private_key)
+        return crypto.sign(private_key, challenge, "sha256")
 
-    if len(keys) > 1:
-        raise IndexError
+    def parse_message(self, message: Dict) -> Tuple[str, Any]:
 
-    variant: str = keys[0]
-    data: Any = message[variant]
-
-    return (variant, data)
-
-
-def authenticate_challenge(challenge: Dict, email: str, model_name: str):
-
-    challenge = challenge["challenge"]
-    print("Authenticating challenge")
-    try:
-        private_key = os.environ["PRIVATE_KEY"]
-        # print(private_key)
-        signed_challenge = sign_challenge(base64.b64decode(challenge), private_key)
-
-        message = {"ChallengeResponse": {"email": email, "model_name": model_name, "response": base64.b64encode(signed_challenge).decode('utf-8') }}
-        
         # print(message)
-        stream.send(json.dumps(message).encode('utf-8'))
-        print("Authenticated")
-    except KeyError:
-        print("Private Key not set in environemt variable")
+        keys: List[str] = list(message)
 
+        if len(keys) > 1:
+            raise IndexError
 
-def display_access(data: Dict):
+        variant: str = keys[0]
+        data: Any = message[variant]
 
-    try:
-        access_token: str = data["token"]
-        model_id: str = data["id"]
+        return (variant, data)
 
-        print("ACCESS TOKEN: {} \n MODEL ID: {}".format(access_token, model_id))
-    except KeyError:
-        print("Malformed data")
+    def authenticate_challenge(self, challenge: Dict):
 
+        challenge = challenge["challenge"]
+        print("Authenticating challenge")
+        try:
+            private_key = os.environ["PRIVATE_KEY"]
+            signed_challenge = self.sign_challenge(
+                base64.b64decode(challenge), private_key
+            )
 
-def verify(email: str, name: str) -> bool:
-    message = {"NewModel": {"email": email, "model_name": name}}
-    stream.send(json.dumps(message).encode('utf-8'))
+            message = {
+                "ChallengeResponse": {
+                    "email": self.email,
+                    "model_name": self.model_name,
+                    "response": base64.b64encode(signed_challenge).decode("utf-8"),
+                }
+            }
 
-    while True:
-        # Read some data
-        data = json.loads(stream.recv(1024))
+            # print(message)
+            self.stream.send(json.dumps(message).encode("utf-8"))
+            print("Authenticated")
+        except KeyError:
+            print("Private Key not set in environemt variable")
 
-        # print("data: {}".format(data))
+    def display_access(self, data: Dict):
 
         try:
-            variant, data = parse_message(data)
+            self.access_token: str = data["token"]
+            self.model_id: str = data["id"]
 
-            if variant == "Challenge":
-                authenticate_challenge(data, email, name)
-            elif variant == "AccessToken":
-                display_access(data)
-                break
-            else:
-                print("Unknown message Variant")
+            print(
+                "ACCESS TOKEN: {} \n MODEL ID: {}".format(
+                    self.access_token, self.model_id
+                )
+            )
+        except KeyError:
+            print("Malformed data")
+        finally:
+            self.stream.close()
 
-        except IndexError:
-            print("Message cannot be parsed")
+    def verify(self) -> bool:
+        message = {"NewModel": {"email": self.email, "model_name": self.model_name}}
+        self.stream.send(json.dumps(message).encode("utf-8"))
 
+        while True:
+            # Read some data
+            data = json.loads(self.stream.recv(1024))
 
+            # print("data: {}".format(data))
+
+            try:
+                variant, data = self.parse_message(data)
+
+                if variant == "Challenge":
+                    self.authenticate_challenge(data)
+                elif variant == "AccessToken":
+                    self.display_access(data)
+                    self.save_access_tokens()
+                    break
+                else:
+                    print("Unknown message Variant")
+
+            except IndexError:
+                print("Message cannot be parsed")
+    
+
+    def save_access_tokens(self):
+
+        path = xdg_data_home() / 'sybl.json'
+
+        key_name = f"{self.email}.{self.model_name}"
+        new_model = {"model_id": self.model_id, "access_token": self.access_token}
+        
+        with path.open('r') as f:
+
+            contents = f.read()
+            json_contents = json.loads(contents if contents else "{}")
+
+            json_contents[key_name] = new_model
+        
+        with path.open('w') as f:
+
+            f.write(json.dumps(json_contents))
+                
 def main():
 
     email: str = input("Enter email: ")
     name: str = input("Enter name of model: ")
     print("Authenticating...")
 
-    verify(email, name)
-    return
+    verifier = Authenication(email, name)
+
+    verifier.verify()
+
 
 if __name__ == "__main__":
     main()
