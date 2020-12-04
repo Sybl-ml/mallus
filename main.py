51229cb2002c389b25d87a0c6adcae4c632ff149
Graham Kemp <grahamk206@gmail.com>
2020-12-04 14:08:04 +0000

    updated main.py as sybl client testing

--- main.py
+++ main.py
@@ -1,25 +1,7 @@
-import socket
-import json
-from typing import List
+from sybl.client import Sybl
 
-DCL_SOCKET: int = 7000
-API_KEY: List[bytes] = b"hello world"
+sybl = Sybl()
 
+sybl.load_model("test1@test.com", "Test")
 
-def heartbeat():
-
-    while True:
-        # Read some data
-        data = json.loads(stream.recv(1024)[:-1])
-
-        print("data: {}".format(data))
-
-        if data == "Alive":
-            stream.send(b"Alive\0")
-
-
-stream = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-stream.connect(("127.0.0.1", DCL_SOCKET))
-stream.send(API_KEY)
-
-heartbeat()
+sybl.connect()
