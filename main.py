81750f9c972deabbb9d5dc5d4c0fcdb423e89bf8
Graham Kemp <grahamk206@gmail.com>
2020-12-06 14:47:50 +0000

    Register callback in testing file

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from sybl.client import Sybl
 
 sybl = Sybl()
 
+def callback(train, predict):
+    return train
+
+sybl.register_callback(callback)
 sybl.load_model("test1@test.com", "Test")
 
 sybl.connect()
