0a8c808a09c0f6dcc448c1964e116c2e7584f26c
Alexander Jackson <alexanderjackson@protonmail.com>
2021-01-18 09:51:05 +0000

    Add another test for empty messages
    Add a test that checks that empty messages will fail to parse.

--- test_authenticate.py
+++ test_authenticate.py
@@ -16,3 +16,10 @@ def test_invalid_messages_fail_to_parse():
 
     with pytest.raises(IndexError):
         variant, data = instance.parse_message(message)
+
+def test_empty_messages_fail_to_parse():
+    instance = Authentication("email", "model_name")
+    message = {}
+
+    with pytest.raises(IndexError):
+        variant, data = instance.parse_message(message)
