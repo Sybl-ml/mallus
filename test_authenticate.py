d2db28083f9494b25afb1389def466c8c782317d
Alexander Jackson <alexanderjackson@protonmail.com>
2021-01-18 16:46:08 +0000

    Fix Pylint issues and socket dependency
    Fix some Pylint issues to bring the score above 8.5 and allow the tests
    to work independent of the DCL running by using `mocket`.

--- test_authenticate.py
+++ test_authenticate.py
@@ -4,8 +4,11 @@ import tempfile
 
 import pytest
 
+from mocket.mocket import mocketize
+
 from authenticate import Authentication
 
+@mocketize
 def test_authenticator_parses_messages_correctly():
     instance = Authentication("email", "model_name")
     message = {"variant": {"key": "value"}}
@@ -14,20 +17,23 @@ def test_authenticator_parses_messages_correctly():
     assert variant == "variant"
     assert data == {"key": "value"}
 
+@mocketize
 def test_invalid_messages_fail_to_parse():
     instance = Authentication("email", "model_name")
     message = {"variant": {"key": "value"}, "another": "value"}
 
     with pytest.raises(IndexError):
-        variant, data = instance.parse_message(message)
+        instance.parse_message(message)
 
+@mocketize
 def test_empty_messages_fail_to_parse():
     instance = Authentication("email", "model_name")
     message = {}
 
     with pytest.raises(IndexError):
-        variant, data = instance.parse_message(message)
+        instance.parse_message(message)
 
+@mocketize
 def test_authentication_creates_sybl_json():
     # Set the environment variable to a temporary directory
     with tempfile.TemporaryDirectory() as directory:
@@ -42,7 +48,8 @@ def test_authentication_creates_sybl_json():
         expected_path = os.path.join(directory, "sybl.json")
         assert os.path.isfile(expected_path)
 
-def test_authentication_creates_sybl_json():
+@mocketize
+def test_authentication_creates_xdg_data_home():
     # Set the environment variable to a temporary directory and delete it
     with tempfile.TemporaryDirectory() as directory:
         os.environ["XDG_DATA_HOME"] = directory
