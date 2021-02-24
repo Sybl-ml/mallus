8e38e92a13b7fcff669e7efad209ca4225c282c0
Graham Kemp <grahamk206@gmail.com>
2021-02-24 08:50:30 +0000

    Pylint and Black fixes

--- tests/test_client.py
+++ tests/test_client.py
@@ -5,27 +5,29 @@ and correct responses to messages sent from the DCL
 
 import os
 import tempfile
-from unittest import mock
 from unittest.mock import Mock
 
 import pytest
 
 from mocket.mocket import mocketize  # type: ignore
 
-from sybl.client import JobConfig, Sybl
+from sybl.client import Sybl
 from sybl.client.sybl_client import State, load_access_token
 from sybl.authenticate import Authentication
+
+# pylint: disable=protected-access
+# pylint: disable=missing-function-docstring
 
 
 @pytest.fixture(scope="function")
 def sybl():
-    sybl = Sybl()
-    sybl._sock = Mock()
-    sybl.model_name = "Test Model"
-    sybl.email = "model@email"
-    sybl._send_message = Mock(name="send_message")
+    sybl_instance = Sybl()
+    sybl_instance._sock = Mock()
+    sybl_instance.model_name = "Test Model"
+    sybl_instance.email = "model@email"
+    sybl_instance._send_message = Mock(name="send_message")
 
-    return sybl
+    return sybl_instance
 
 
 @pytest.fixture
@@ -46,84 +48,90 @@ def valid_dataset():
         }
     }
 
-
-def test_valid_connect(sybl):
-    sybl._access_token = "12345"
-    sybl._model_id = "45678"
-    sybl.callback = Mock()
-    sybl._is_authenticated = Mock(name="_is_authenticated", return_value=True)
-    sybl._begin_state_machine = Mock(name="_begin_state_machine")
-
-    assert sybl._state == State.AUTHENTICATING
-
-    sybl.connect()
-
-    assert sybl._state == State.HEARTBEAT
-
-    assert sybl._is_authenticated.called
-    assert sybl._begin_state_machine.called
+    return dataset
 
 
-def test_unauthenticated_connect(sybl):
-    sybl._access_token = "12345"
-    sybl._model_id = "45678"
-    sybl.callback = Mock()
-    sybl._is_authenticated = Mock(name="_is_authenticated", return_value=False)
+def test_valid_connect(sybl_instance):
+    sybl_instance._access_token = "12345"
+    sybl_instance._model_id = "45678"
+    sybl_instance.callback = Mock()
+    sybl_instance._is_authenticated = Mock(name="_is_authenticated", return_value=True)
+    sybl_instance._begin_state_machine = Mock(name="_begin_state_machine")
+
+    assert sybl_instance._state == State.AUTHENTICATING
+
+    sybl_instance.connect()
+
+    assert sybl_instance._state == State.HEARTBEAT
+
+    assert sybl_instance._is_authenticated.called
+    assert sybl_instance._begin_state_machine.called
+
+
+def test_unauthenticated_connect(sybl_instance):
+    sybl_instance._access_token = "12345"
+    sybl_instance._model_id = "45678"
+    sybl_instance.callback = Mock()
+    sybl_instance._is_authenticated = Mock(name="_is_authenticated", return_value=False)
 
     with pytest.raises(
         PermissionError, match="Model access token has not been authenticated"
     ):
-        sybl.connect()
+        sybl_instance.connect()
 
-    assert sybl._state == State.AUTHENTICATING
+    assert sybl_instance._state == State.AUTHENTICATING
 
 
-def test_no_callback_connect(sybl):
+def test_no_callback_connect(sybl_instance):
 
     with pytest.raises(AttributeError, match="Callback has not been registered"):
-        sybl.connect()
+        sybl_instance.connect()
 
-    assert sybl._state == State.AUTHENTICATING
+    assert sybl_instance._state == State.AUTHENTICATING
 
 
-def test_no_model_id_connect(sybl):
-    sybl.callback = Mock()
+def test_no_model_id_connect(sybl_instance):
+    sybl_instance.callback = Mock()
 
     with pytest.raises(
         AttributeError, match="Model access token and ID have not been loaded"
     ):
-        sybl.connect()
+        sybl_instance.connect()
 
-    assert sybl._state == State.AUTHENTICATING
-
-
-def test_authentication(sybl):
-    sybl._read_message = Mock(return_value={"message": "Authentication successful"})
-
-    assert sybl._is_authenticated()
+    assert sybl_instance._state == State.AUTHENTICATING
 
 
-def test_not_authentication(sybl):
-    sybl._read_message = Mock(return_value={"message": "Authentication unsuccessful"})
+def test_authentication(sybl_instance):
+    sybl_instance._read_message = Mock(
+        return_value={"message": "Authentication successful"}
+    )
 
-    assert not sybl._is_authenticated()
-
-
-def test_bad_response_authentication(sybl):
-    sybl._read_message = Mock(return_value={"bad": "response"})
-
-    assert not sybl._is_authenticated()
+    assert sybl_instance._is_authenticated()
 
 
-def test_reject_bad_config(sybl):
+def test_not_authentication(sybl_instance):
+    sybl_instance._read_message = Mock(
+        return_value={"message": "Authentication unsuccessful"}
+    )
+
+    assert not sybl_instance._is_authenticated()
+
+
+def test_bad_response_authentication(sybl_instance):
+    sybl_instance._read_message = Mock(return_value={"bad": "response"})
+
+    assert not sybl_instance._is_authenticated()
+
+
+def test_reject_bad_config(sybl_instance):
 
     bad_config = "Im not a JobConfig"
 
     with pytest.raises(AttributeError):
-        sybl.load_config(bad_config)
+        sybl_instance.load_config(bad_config)
 
 
-def test_heartbeat(sybl):
+def test_heartbeat(sybl_instance):
 
     responses = [
         ({"Alive": ""}, State.HEARTBEAT),
@@ -132,57 +140,45 @@ def test_heartbeat(sybl):
     ]
 
     for response in responses:
-        sybl._read_message = Mock(return_value=response[0])
+        sybl_instance._read_message = Mock(return_value=response[0])
 
-        sybl._heartbeat()
-        assert sybl._state == response[1]
-
-
-def test_accept_job_config(sybl):
-
-    sybl._message_stack.append({"JobConfig": {"Test config"}})
-    sybl.config.compare = Mock(return_value=True)
-
-    sybl._process_job_config()
-
-    sybl._send_message.assert_called_with({"ConfigResponse": {"accept": True}})
-    assert sybl._state == State.HEARTBEAT
+        sybl_instance._heartbeat()
+        assert sybl_instance._state == response[1]
 
 
-def test_reject_job_config(sybl):
+def test_accept_job_config(sybl_instance):
 
-    sybl._message_stack.append({"JobConfig": {"Test config"}})
-    sybl.config.compare = Mock(return_value=False)
+    sybl_instance._message_stack.append({"JobConfig": {"Test config"}})
+    sybl_instance.config.compare = Mock(return_value=True)
 
-    sybl._process_job_config()
+    sybl_instance._process_job_config()
 
-    sybl._send_message.assert_called_with({"ConfigResponse": {"accept": False}})
-    assert sybl._state == State.HEARTBEAT
-
-
-def test_empty_message_stack(sybl):
-
-    sybl._process_job_config()
-
-    assert sybl._state == State.HEARTBEAT
+    sybl_instance._send_message.assert_called_with({"ConfigResponse": {"accept": True}})
+    assert sybl_instance._state == State.HEARTBEAT
 
 
-@pytest.fixture(scope="function")
-@mocketize
-def temporary_access_file(tmpdir_factory):
-    directory = tmpdir_factory.mktemp("xdg")
-    os.environ["XDG_DATA_HOME"] = str(directory)
+def test_reject_job_config(sybl_instance):
 
-    instance = Authentication("model@email", "Test Model")
-    instance.access_token = ""
-    instance.model_id = "2344423"
-    instance.save_access_tokens()
+    sybl_instance._message_stack.append({"JobConfig": {"Test config"}})
+    sybl_instance.config.compare = Mock(return_value=False)
 
-    return directory
+    sybl_instance._process_job_config()
+
+    sybl_instance._send_message.assert_called_with(
+        {"ConfigResponse": {"accept": False}}
+    )
+    assert sybl_instance._state == State.HEARTBEAT
+
+
+def test_empty_message_stack(sybl_instance):
+
+    sybl_instance._process_job_config()
+
+    assert sybl_instance._state == State.HEARTBEAT
 
 
 @mocketize
-def test_load_access_token(sybl):
+def test_load_access_token(sybl_instance):
 
     with tempfile.TemporaryDirectory() as directory:
         os.environ["XDG_DATA_HOME"] = directory
@@ -191,14 +187,16 @@ def test_load_access_token(sybl):
         instance.model_id = "2344423"
         instance.save_access_tokens()
 
-        access_token, model_id = load_access_token(sybl.email, sybl.model_name)
+        access_token, model_id = load_access_token(
+            sybl_instance.email, sybl_instance.model_name
+        )
 
     assert access_token == ""
     assert model_id == "2344423"
 
 
 @mocketize
-def test_bad_model_name(sybl):
+def test_bad_model_name(sybl_instance):
 
     with tempfile.TemporaryDirectory() as directory:
         os.environ["XDG_DATA_HOME"] = directory
@@ -208,12 +206,13 @@ def test_bad_model_name(sybl):
         instance.save_access_tokens()
 
         with pytest.raises(
-            ValueError, match=f"Model Nothing here not registered to {sybl.email}"
+            ValueError,
+            match=f"Model Nothing here not registered to {sybl_instance.email}",
         ):
-            load_access_token(sybl.email, "Nothing here")
+            load_access_token(sybl_instance.email, "Nothing here")
 
 
-def test_file_does_not_exist(sybl):
+def test_file_does_not_exist(sybl_instance):
 
     with pytest.raises(FileNotFoundError):
-        load_access_token(sybl.email, sybl.model_name)
+        load_access_token(sybl_instance.email, sybl_instance.model_name)
